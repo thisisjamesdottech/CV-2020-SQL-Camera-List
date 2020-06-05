543edd366ef4c16750f99a2eee6b5987248dab87
thisisjamesdottech <me@thisisjames.tech>
2020-06-04 22:56:22 -0500

    Create CV-2020-SQL-Camera-List.sql
    Added code to Master

---
+++ CV-2020-SQL-Camera-List.sql
@@ -0,0 +1,25 @@
+/*
+Name:       CompleteView 2020 Camera List
+Purpose:    Produces a report of all cameras in the CompleteView database and their associated make, model,
+			IP address, and MAC address
+Author:     James Anderson
+Date:       19May2020
+Version:    1.3
+Comments:   Cameras with multiple channels will have their own line item but share the same IP address; the first entry for that camera, regardless of channel count, may be the only camera to possess the MAC address information, but this isn't always the case
+
+Version History:
+ -  :		(16Apr2020) 1.0 Initial testing of code and assignment of column names
+ -  :		(17Apr2020) 1.1 Added Make and Model columns to query
+ -  :		(27Apr2020) 1.2 Added # of Channels, Channel ID, and MAC Address columns to query, changed Name to "CompleteView 2020 Camera List," updated Purpose, and added Comment regarding MAC Address records
+ -  :		(19May2020) 1.3 Changed v.IsDeleted to c.IsDeleted to create a cleaner list of cameras that are not deleted
+*/
+SELECT a.CameraName AS 'Camera Name',v.Manufacturer AS 'Make',v.Model AS 'Model',v.ChannelCount AS '# of Channels',c.ChannelId AS 'Channel ID',v.IpAddressV4 AS 'IP Address',v.MacAddress AS 'MAC Address'
+FROM CompleteView.dbo.VideoDevices v
+INNER JOIN CompleteView.dbo.Device d
+ON v.DeviceRelationId=d.DeviceRelationId
+INNER JOIN CompleteView.dbo.CameraVideoDevice c
+ON d.DeviceId=c.DeviceId
+INNER JOIN CompleteView.dbo.Camera a
+ON c.CameraRelationId=a.CameraRelationId
+-- Below IsDeleted = 0 will remove any cameras that have been deleted from the database
+WHERE c.IsDeleted = 0
